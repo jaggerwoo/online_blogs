5ac014a1bd7b0d2516357f30a5324b4754e26f18
Wujg <wu594271610@gmail.com>
2018-02-26 17:34:02 +0800

    bug

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -135,3 +135,81 @@ after_fork do |server, worker|
     ActiveRecord::Base.establish_connection
   end
 end
+
+# user root;
+# worker_processes 2;
+# error_log /var/log/nginx/error.log;
+# pid /run/nginx.pid;
+
+# events {
+#     worker_connections 1024;
+# }
+
+# http {
+#     log_format  main  '$remote_addr - $remote_user [$time_local] "$request" '
+#                       '$status $body_bytes_sent "$http_referer" '
+#                       '"$http_user_agent" "$http_x_forwarded_for"';
+
+#     access_log  /var/log/nginx/access.log  main;
+
+#     sendfile            on;
+#     tcp_nopush          on;
+#     tcp_nodelay         on;
+#     keepalive_timeout   30;
+#     types_hash_max_size 2048;
+
+#     include             /etc/nginx/mime.types;
+#     default_type        application/octet-stream;
+
+
+#     underscores_in_headers    on;
+
+#     # Load modular configuration files from the /etc/nginx/conf.d directory.
+#     # See http://nginx.org/en/docs/ngx_core_module.html#include
+#     # for more information.
+#     include /etc/nginx/conf.d/*.conf;
+
+#     gzip  on;
+#     gzip_http_version 1.1;
+#     gzip_vary on;
+#     gzip_comp_level 5;
+#     gzip_types text/css application/x-javascript application/javascript text/javascript application/json;
+#     gzip_buffers 16 8k;
+
+#     client_max_body_size 20m;
+
+#     upstream app {
+#         server localhost:3000 fail_timeout=0s;
+#     }
+
+#     server {
+#        listen 80;
+#        server_name 47.104.2.156;
+
+#         error_page   500 502 503 504  /50x.html;
+#         location = /50x.html {
+#             root   html;
+#         }
+
+#         # root /root/cities_in_japan/public;
+#   root /home/deploy/online_blogs/current/public;
+
+#         # try_files will find the $uri in the given file list, if not found it will hand it to @app handler
+#         try_files $uri/index.html $uri @app; 
+
+#         location ~* \.(xml|txt|html|htm|ico|css|js|gif|jpe?g|png)(\?[0-9]+)?$ {
+#             expires max;
+#             try_files $uri/index.html $uri @app; 
+#         }
+
+#         location @app {
+#             proxy_set_header   Host $host;
+#             proxy_set_header   X-Forwarded-Host $host;
+#             proxy_set_header   X-Forwarded-Server $host;
+#             proxy_set_header   X-Real-IP        $remote_addr;
+#             proxy_set_header   X-Forwarded-For  $proxy_add_x_forwarded_for;
+#             proxy_buffering    on;
+#             proxy_pass         http://127.0.0.1:3000;
+#        }
+#     }
+# }
