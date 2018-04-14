7351109129f7683f6fbdcc92b24ab9a72058d557
吴嘉赓 <wu594271610gmail.com>
2018-04-15 01:00:19 +0800

    bug

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,17 +2,17 @@ class WelcomeController < ApplicationController
   
   def index
     @tags = Tag.all.distinct
-    @blogs = Blog.order('id desc').page(params[:page] || 1).per_page(params[:per_page] || 10)
+    @blogs = Blog.order('id desc').page(params[:page] || 1).per_page(params[:per_page] || 9)
   end
 
   def search_blog
     find_tag = Tag.find params[:tag_id]
-    @blogs = find_tag.blogs.order('id desc').page(params[:page] || 1).per_page(params[:per_page] || 10)
+    @blogs = find_tag.blogs.order('id desc').page(params[:page] || 1).per_page(params[:per_page] || 9)
     @tags = Tag.all.distinct
   end
 
   def search
-    @blogs = Blog.where("title like :title", title: "%#{params[:w]}%").order('id desc').page(params[:page] || 1).per_page(params[:per_page] || 10)
+    @blogs = Blog.where("title like :title", title: "%#{params[:w]}%").order('id desc').page(params[:page] || 1).per_page(params[:per_page] || 9)
     @tags = Tag.all.distinct
     render action: :index
   end
