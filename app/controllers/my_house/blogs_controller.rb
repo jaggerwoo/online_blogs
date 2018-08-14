1ae65e68624aab308ca21ce7f6b241937fe94fa4
Woo <wu594271610@gmail.com>
2018-08-14 17:12:08 +0800

    minor

--- app/controllers/my_house/blogs_controller.rb
+++ app/controllers/my_house/blogs_controller.rb
@@ -1,4 +1,5 @@
 class MyHouse::BlogsController < MyHouse::BaseController
+  skip_before_filter :verify_authenticity_token, :only => [:create]
   before_action :find_blog_id, except: [:new, :create, :index]
   
   def new
