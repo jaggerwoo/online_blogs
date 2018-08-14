7ff47a711e8f1396c6fe6862ff59f76eca1ae2de
Woo <wu594271610@gmail.com>
2018-08-14 17:26:23 +0800

    minor

--- app/controllers/my_house/base_controller.rb
+++ app/controllers/my_house/base_controller.rb
@@ -1,5 +1,6 @@
 class MyHouse::BaseController < ActionController::Base
   protect_from_forgery with: :exception
+  skip_before_filter :verify_authenticity_token, :if => Proc.new { |c| c.request.format == 'application/json' }
   layout 'my_house/layouts/my_house'
 
   include Signable::UserSession
