eb48ed08a6eafe0a1504fc7d4e49825af284239f
吴嘉赓 <wu594271610gmail.com>
2018-08-18 20:10:19 +0800

    minor

--- config/deploy.rb
+++ config/deploy.rb
@@ -28,7 +28,7 @@ set :conditionally_migrate, true
 
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml"
+# append :linked_files, "config/database.yml", "config/secrets.yml"
 
 # Default value for linked_dirs is []
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sessions", "tmp/sockets", "public/system"
